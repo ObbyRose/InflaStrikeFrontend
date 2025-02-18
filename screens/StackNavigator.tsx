dce99af99f402a72456661ab0ed3cf3c31b9b995
Tal Maman <dreapet41s@gmail.com>
2025-02-18 16:05:26 +0200

    spaceing for readabillity

--- screens/StackNavigator.tsx
+++ screens/StackNavigator.tsx
@@ -13,30 +13,30 @@ import Layout from './Layout';
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Layout>
-        <Stack.Navigator initialRouteName="Notifications">
-          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-          <Stack.Screen
-            name="Investment"
-            component={InvestmentsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="Notifications" component={NotificationsScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="Savings" component={SavingsScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Token" component={TokenScreen} />
-          <Stack.Screen
-            name="Transactions"
-            component={TransactionsScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </Layout>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Layout>
+                <Stack.Navigator initialRouteName="Notifications">
+                    <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                    <Stack.Screen
+                        name="Investment"
+                        component={InvestmentsScreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen name="Notifications" component={NotificationsScreen} options={{ headerShown: false }} />
+                    <Stack.Screen name="Profile" component={ProfileScreen} />
+                    <Stack.Screen name="Savings" component={SavingsScreen} options={{ headerShown: false }}/>
+                    <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false }} />
+                    <Stack.Screen name="Token" component={TokenScreen} />
+                    <Stack.Screen
+                        name="Transactions"
+                        component={TransactionsScreen}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </Layout>
+        </NavigationContainer>
+    );
 };
 
 export default StackNavigator;
