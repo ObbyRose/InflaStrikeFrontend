adf3e8957bf6cc7aecbd08348a12d4541fe76ffb
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-02-20 14:08:33 +0200

    init login signup screens

---
+++ screens/auth/Signup.tsx
@@ -0,0 +1,11 @@
+import { Props } from '@/types/NavigationTypes'
+import React from 'react'
+
+
+const Signup: React.FC<Props> = ({ navigation }) => {
+    return (
+        <div>Signup</div>
+    )
+}
+
+export default Signup
