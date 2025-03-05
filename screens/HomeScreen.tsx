8588af8cfbe29d61658f4b1ef22d18f8c59b5667
Tal Maman <dreapet41s@gmail.com>
2025-03-05 15:33:34 +0200

    refactor: update initial route in StackNavigator and enhance HomeScreen layout

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Text, ScrollView, TouchableOpacity } from "react-native";
 import { Box } from "@/components/ui/box";
-import { Avatar } from "@/components/ui/avatar";
-import { Icon } from "@/components/ui/icon";
-import { User } from "lucide-react-native";
 import { useTheme } from "@/utils/Themes/ThemeProvider";
 import { Divider } from "@/components/ui/divider";
 import { useUserStore } from "@/context/userStore"; // Import the Zustand store
-import { IC_Bitcoin, IC_Ethereum, IC_Xrp, IC_Settings, IC_NotificationsInactive } from "@/utils/constants/Icons";
+import { IC_Bitcoin, IC_Ethereum, IC_Xrp, IC_Bell_White, IC_Info, IC_Top_Up, IC_Transaction, IC_History, IC_Tothor } from "@/utils/constants/Icons";
 import CryptoMarketCard from "@/components/CryptoMarketCard";
 import {
     fetchBitcoinLivePrice,
@@ -21,6 +18,8 @@ import {
     fetchXRPLineData
 } from "../utils/api/external/BinanceAPI";
 import { Props } from "@/types/NavigationTypes";
+import MyLinearGradient from "@/components/gradient/MyLinearGradient";
+import RoundedBox from "@/components/RoundedBox";
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
     const { appliedTheme } = useTheme();
@@ -92,72 +91,81 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
             {/* Header Section */}
-            <Box className={`p-4 bg-background-${appliedTheme} h-full flex gap-2`}>
-                <Box className="flex-row justify-between items-center mt-4">
-                    <TouchableOpacity onPress={() => navigation.navigate("MainApp", { screen: "Profile" })} activeOpacity={0.7}>
-                        <Avatar className="bg-indigo-600" size="lg">
-                            <Icon as={User} size="xl" className="stroke-white" />
-                        </Avatar>
-                    </TouchableOpacity>
-
-                    <Box className="flex-row items-center gap-2 mt-3">
-                        <TouchableOpacity onPress={() => navigation.navigate("MainApp", { screen: "Notifications" })}>
-                            <Box className="flex-col items-center gap-1">
-                                <IC_NotificationsInactive className="h-7 w-7" />
-                                <Text className="text-2xs font-semibold text-[#969AA0]">Notifications</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => navigation.navigate("SettingsStack", { screen: "Settings" })}>
-                            <Box className="flex-col items-center gap-1">
-                                <IC_Settings className="h-7 w-7" />
-                                <Text className="ml-2 text-2xs font-semibold text-[#969AA0]">Settings</Text>
-                            </Box>
-                        </TouchableOpacity>
+            <MyLinearGradient type="background" color={appliedTheme === 'dark' ? 'dark' : 'purple'}>
+                <Box className="h-[40%] p-4 gap-2">
+                    <Box className="flex flex-row items-center justify-between">
+                        <IC_Tothor className="w-40" />
+                        <IC_Bell_White className="w-7 h-7" />
                     </Box>
-                </Box>
-
-                {/* Greetings Section */}
-                <Box className="mb-4">
-                    <Text className="text-[36px] mt-2 text-black font-bold">Hello,</Text>
-                    <Text className="text-[36px] text-black font-bold">{user ? `${user.fName} ${user.lName}` : "User"} 👋</Text>
-                </Box>
-
-                {/* Net Liquid Value (NLVE) Section */}
-                <Box className={`bg-indigo-600 p-2 h-[100px] w-full rounded-3xl flex`}>
-                    <Box className="p-2">
-                        <Text className="text-white text-[18px]">Total Balance:</Text>
-                        <Box>
-                            <Text className="text-white text-[20px] self-end">
-                                ${user?.nlve ? parseFloat(user.nlve).toLocaleString() : "0.00"}
-                            </Text>
+                    <Box className="flex flex-col gap-4">
+                        <Text className="text-white font-medium text-[17px]">Safe-to-spend</Text>
+                        <Box className="flex-row gap-4 items-center">
+                        <Text className="text-white font-medium text-4xl">$ 2,987.56</Text>
+                        <IC_Info className="w-6 h-6" />
                         </Box>
-                        <Text className="text-[12px] text-white top-0">Cash Available</Text>
+                        <Text className="text-white text-[14px]">Updated 2 mins ago</Text>
+                    </Box>
+
+                    <Box className="items-center bottom-0 mt-6">
+                        <Box className={`bg-card-${appliedTheme} p-2 border-2 border-slate-500 rounded-full w-[80%] z-10`}>
+                            <Box className={`flex flex-row justify-evenly`}>
+                                <Box className="flex-col items-center text-center">
+                                    <IC_Top_Up className="w-12 h-12"/>
+                                    <Text className={`text-text-${appliedTheme} font-bold`}> Top up</Text>
+                                </Box>
+                                <Box className="p-2">
+                                    <Box className={` border-s-[1px] border-divider-${appliedTheme} h-full justify-center  mx-2`} />
+                                </Box>
+                                <Box className="flex-col items-center text-center">
+                                    <IC_Transaction className="w-12 h-12"/>
+                                    <Text className={`text-text-${appliedTheme} font-bold`}> Transfer </Text>
+                                </Box>
+                                <Box className="p-2">
+                                    <Box className={` border-s-[1px] border-divider-${appliedTheme} h-full justify-center  mx-2`} />
+                                </Box>
+                                <Box className="flex-col items-center text-center">
+                                    <IC_History className="w-12 h-12"/>
+                                    <Text className={`text-text-${appliedTheme} font-bold`}> History </Text>
+                                </Box>
+                            </Box>
+                        </Box>
                     </Box>
                 </Box>
-
-                {/* Crypto Market Section */}
+                </MyLinearGradient>
+            <MyLinearGradient type="background" color={appliedTheme === 'dark' ? 'dark' : 'light-blue'}>
+                <Box className={`p-4 bg-background-${appliedTheme} h-full`}>
+                {/* Recommended Bundles Section */}
                 <Box>
-                    <Box className="flex-row items-center justify-between mt-4">
-                        <Text className="text-[22px] font-semibold">Markets</Text>
+                    <Box className="flex-row items-center justify-between mt-12">
+                        <Text className="text-[22px] font-semibold">Recommended Bundles</Text>
                         <Text className="text-[14px] text-[#0A6CFF]" onPress={() => navigation.navigate("MainApp", { screen: "Markets" })}>
                             See All
                         </Text>
                     </Box>
                 </Box>
 
-                {/* Crypto Market Cards */}
-                {loading ? (
-                    <Text>Loading...</Text>
-                ) : (
-                    cryptoData.map((crypto, index) => (
-                        <React.Fragment key={crypto.symbol}>
-                            <CryptoMarketCard {...crypto} />
-                            {index < cryptoData.length - 1 && <Divider className={`bg-divider-${appliedTheme}`} />}
-                        </React.Fragment>
-                    ))
-                )}
-            </Box>
+                    {/* Crypto Market Section */}
+                    <Box>
+                        <Box className="flex-row items-center justify-between mt-12">
+                            <Text className="text-[22px] font-semibold">Markets</Text>
+                            <Text className="text-[14px] text-[#0A6CFF]" onPress={() => navigation.navigate("MainApp", { screen: "Markets" })}>
+                                See All
+                            </Text>
+                        </Box>
+                    </Box>
+                    {/* Crypto Market Cards */}
+                    {loading ? (
+                        <Text>Loading...</Text>
+                    ) : (
+                        cryptoData.map((crypto, index) => (
+                            <React.Fragment key={crypto.symbol}>
+                                <CryptoMarketCard {...crypto} />
+                                {index < cryptoData.length - 1 && <Divider className={`bg-divider-${appliedTheme}`} />}
+                            </React.Fragment>
+                        ))
+                    )}
+                </Box>
+            </MyLinearGradient>
         </ScrollView>
     );
 };
