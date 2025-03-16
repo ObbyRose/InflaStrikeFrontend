c5e87d6ac158bce1c8633e813b7a817497f67386
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2025-03-16 17:38:08 +0200

    Refactor BuySellSheet component for improved readability and add BuyNSell integration

--- components/BuyNSell.tsx
+++ components/BuyNSell.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Box } from './ui/box';
 import { Text } from './ui/text';
-import { Alert, TextInput, TouchableOpacity } from 'react-native';
+import {
+  Alert,
+  Keyboard,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { useTheme } from '@/utils/Themes/ThemeProvider';
 import {
   Select,
@@ -17,29 +23,43 @@ import {
 } from '@/components/ui/select';
 import { ChevronDownIcon } from './ui/icon';
 import { IC_Minus, IC_Plus } from '@/utils/constants/Icons';
+import ButtonsTrain from './ButtonsTrain';
 
 interface BuyNSellProps {
   currentCurrencyValue: string;
   currentCurrencySymbol: string;
+  onClose: () => void;
 }
 
-export default function BuyNSell({ currentCurrencyValue, currentCurrencySymbol }: BuyNSellProps) {
-  const [isAtBuying, setIsAtBuying] = useState(true);
+export default function BuyNSell({
+  currentCurrencyValue,
+  currentCurrencySymbol,
+  onClose,
+}: BuyNSellProps) {
+  const [buyOrSell, setBuyOrSell] = useState('Buy');
   const [transactionType, setTransactionType] = useState<string>('');
   const [USDTamount, setUSDTamount] = useState<string>('');
   const numericCurrencyValue = parseFloat(currentCurrencyValue);
   const newCurrencyAmount = USDTamount
     ? (parseFloat(USDTamount) / numericCurrencyValue).toFixed(6)
-    : '0'; // derived from the USDT amount state
+    : ''; // derived from the USDT amount state
 
   const { appliedTheme } = useTheme();
 
-  const userCurrentCurrency = 1000000; //! need to be fetch from the user
+  const userCurrentCurrency = 24; //! need to be fetch from the user
   const userCurrentUSDT = 2000000; //! need to be fetch from the user
 
   function handleUSDTamountChange(value: string) {
-    if (value === '-1') {
-      Alert.alert(`Invalid amount. Please enter a valid amount from 0 to ${userCurrentUSDT}`);
+    const limit =
+      buyOrSell === 'Buy'
+        ? userCurrentUSDT // Buying: Maximum USDT available
+        : userCurrentCurrency * numericCurrencyValue; // Selling: Maximum current currency converted to USDT
+    const alertMessage = `Invalid amount. Please enter a valid amount from 0 to ${limit.toFixed(2)}`;
+
+    // if trying to go to minus - reset to 0
+    if (value[0] === '-') {
+      Alert.alert(alertMessage);
+      setUSDTamount('');
       return;
     }
 
@@ -51,10 +71,60 @@ export default function BuyNSell({ currentCurrencyValue, currentCurrencySymbol }
     }
     const numericValue = parseFloat(value);
 
-    if (!isNaN(numericValue) && numericValue <= userCurrentUSDT && numericValue >= 0) {
-      setUSDTamount(value);
+    if (!isNaN(numericValue)) {
+      if (numericValue <= limit) {
+        setUSDTamount(value);
+      } else {
+        setUSDTamount(limit.toFixed(2).toString());
+        Alert.alert(alertMessage);
+      }
     } else {
-      Alert.alert(`Invalid amount. Please enter a valid amount from 0 to ${userCurrentUSDT}`);
+      Alert.alert(alertMessage);
+    }
+  }
+
+  function handleCurrentCurrencyAmountChange(value: string) {
+    const limit =
+      buyOrSell === 'Buy'
+        ? userCurrentUSDT / numericCurrencyValue // Buying: Maximum USDT available
+        : userCurrentCurrency; // Selling: Maximum BTC converted to USDT
+
+    const alertMessage = `Invalid amount. Please enter a valid amount from 0 to ${limit}`;
+
+    // if trying to go to minus - reset to 0
+    if (value[0] === '-') {
+      Alert.alert(alertMessage);
+      setUSDTamount('');
+      return;
+    }
+    value = value.replace(/[^\d.]/g, '').replace(/\.(?=.*\.)/g, ''); // remove all non numeric characters
+
+    // if empty or 0 - reset to 0
+    if (value === '' || value === '0') {
+      setUSDTamount('');
+      return;
+    }
+
+    const numericValue = parseFloat(value);
+
+    if (!isNaN(numericValue)) {
+      let calculatedUSDT = '';
+      if (numericValue <= limit) {
+        calculatedUSDT = (numericValue * numericCurrencyValue).toFixed(2); // Keeping 2 decimal places
+      } else {
+        calculatedUSDT = (limit * numericCurrencyValue).toFixed(2); // Keeping 2 decimal places
+        Alert.alert(alertMessage);
+      }
+      //   console.log(`baba is : ${value}`);
+      //   console.log('calculatedUSDT', calculatedUSDT);
+
+      // } numericValue >= 0 && numericValue <= maxCurrencyPossibleForBuying) {
+      // Convert currency back to USDT
+      //   const calculatedUSDT = (numericValue * numericCurrencyValue).toFixed(2); // Keeping 2 decimal places
+
+      setUSDTamount(calculatedUSDT);
+    } else {
+      Alert.alert(alertMessage);
     }
   }
 
@@ -67,67 +137,139 @@ export default function BuyNSell({ currentCurrencyValue, currentCurrencySymbol }
       Alert.alert('Please enter the amount');
       return;
     }
-    Alert.alert(
-      `congratulations! You have successfully bought ${newCurrencyAmount} ${currentCurrencySymbol}`
-    );
+    if (buyOrSell === 'buy') {
+      Alert.alert(
+        `congratulations! You have successfully bought ${newCurrencyAmount} ${currentCurrencySymbol}`
+      );
+    } else {
+      Alert.alert(
+        `congratulations! You have successfully sold ${newCurrencyAmount} ${currentCurrencySymbol}`
+      );
+    }
+    onClose();
   }
+
+  function handleSetBuyOrSell(value: string) {
+    setTransactionType('');
+    setUSDTamount('');
+    setBuyOrSell(value);
+  }
+
+  console.log(transactionType);
 
   return (
     <>
-      <Box className="gap-4">
-        {/* Buy and Sell buttons */}
-        <Box className="h-fit w-full flex-row p-2">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <Box className="h-full justify-around gap-4 p-2">
+          {/* Buy and Sell buttons */}
+          <Box className="h-fit w-full flex-row justify-around p-2">
+            <TouchableOpacity
+              className={`${buyOrSell === 'Buy' ? 'bg-green-500' : `bg-card-${appliedTheme}`} w-1/2 items-center rounded-l-xl p-4`}
+              onPress={() => handleSetBuyOrSell('Buy')}>
+              <Text className={buyOrSell === 'Buy' ? 'text-white' : `text-text-${appliedTheme}`}>
+                Buy
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className={`${buyOrSell === 'Buy' ? `bg-card-${appliedTheme}` : 'bg-red-500'} w-1/2 items-center rounded-r-xl p-4`}
+              onPress={() => handleSetBuyOrSell('Sell')}>
+              <Text className={buyOrSell === 'Buy' ? `text-text-${appliedTheme}` : 'text-white'}>
+                Sell
+              </Text>
+            </TouchableOpacity>
+          </Box>
+
+          {/* transaction type input choosing */}
+          <Select
+            selectedValue={transactionType ? transactionType : ''}
+            onValueChange={(value) => setTransactionType(value)}
+            className={`bg-card-${appliedTheme}`}>
+            <SelectTrigger variant="outline" size="md">
+              <SelectInput
+                placeholder="Select transaction type"
+                className={`text-text-${appliedTheme} `}
+              />
+              <SelectIcon className="mr-3" as={ChevronDownIcon} />
+            </SelectTrigger>
+            <SelectPortal>
+              <SelectBackdrop />
+              <SelectContent>
+                <SelectDragIndicatorWrapper>
+                  <SelectDragIndicator />
+                </SelectDragIndicatorWrapper>
+                <SelectItem label="Select transaction type" value="" isDisabled={true} />
+                <SelectItem label="FOK" value="FOK" />
+              </SelectContent>
+            </SelectPortal>
+          </Select>
+
+          {/* USDT amount input  */}
+          <Box>
+            <Text className={` text-subText-${appliedTheme} mb-2`}>
+              How much USDT you want to {buyOrSell === 'Buy' ? 'sell' : 'buy'}:
+            </Text>
+            <Box className={`flex-row items-center justify-around gap-2 rounded-lg  border`}>
+              <TouchableOpacity
+                onPress={() =>
+                  handleUSDTamountChange((+(USDTamount ? USDTamount : 0) - 1).toString())
+                }>
+                <IC_Minus className="h-4 w-4" color={appliedTheme === 'dark' ? '#ffffff' : ''} />
+              </TouchableOpacity>
+              <TextInput
+                className={`text-text-${appliedTheme} `}
+                value={USDTamount}
+                keyboardType="numeric"
+                placeholder="USDT"
+                onChangeText={handleUSDTamountChange}
+              />
+              <TouchableOpacity
+                onPress={() =>
+                  handleUSDTamountChange((+(USDTamount ? USDTamount : 0) + 1).toString())
+                }>
+                <IC_Plus className="h-4 w-4" color={appliedTheme === 'dark' ? '#ffffff' : ''} />
+              </TouchableOpacity>
+            </Box>
+          </Box>
+
+          {/* current currency amount input  */}
+          <Box className="">
+            <Text className={` text-subText-${appliedTheme} mb-2`}>
+              How much {currentCurrencySymbol} you want to {buyOrSell === 'Buy' ? 'buy' : 'sell'}:
+            </Text>
+            <Box className={`flex-row items-center justify-around gap-2  rounded-lg border`}>
+              <TouchableOpacity
+                onPress={() =>
+                  handleCurrentCurrencyAmountChange(
+                    (+(newCurrencyAmount ? newCurrencyAmount : 0) - 1).toString()
+                  )
+                }>
+                <IC_Minus className="h-4 w-4" color={appliedTheme === 'dark' ? '#ffffff' : ''} />
+              </TouchableOpacity>
+              <TextInput
+                className={`text-text-${appliedTheme} `}
+                value={newCurrencyAmount}
+                keyboardType="numeric"
+                placeholder={currentCurrencySymbol}
+                onChangeText={handleCurrentCurrencyAmountChange}
+              />
+              <TouchableOpacity
+                onPress={() =>
+                  handleCurrentCurrencyAmountChange(
+                    (+(newCurrencyAmount ? newCurrencyAmount : 0) + 1).toString()
+                  )
+                }>
+                <IC_Plus className="h-4 w-4" color={appliedTheme === 'dark' ? '#ffffff' : ''} />
+              </TouchableOpacity>
+            </Box>
+          </Box>
+
           <TouchableOpacity
-            className={`${isAtBuying ? 'bg-green-500' : `bg-card-${appliedTheme}`} p-2`}
-            onPress={() => setIsAtBuying(true)}>
-            <Text className={isAtBuying ? 'text-white' : `text-text-${appliedTheme}`}>Buy</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className={`${isAtBuying ? `bg-card-${appliedTheme}` : 'bg-red-500'} p-2`}
-            onPress={() => setIsAtBuying(false)}>
-            <Text className={isAtBuying ? `text-text-${appliedTheme}` : 'text-white'}>Sell</Text>
+            className="items-center rounded-lg bg-green-500 p-2"
+            onPress={handleBuying}>
+            <Text className="text-white">{buyOrSell}</Text>
           </TouchableOpacity>
         </Box>
-
-        {/* transaction type input choosing */}
-        <Select onValueChange={(value) => setTransactionType(value)}>
-          <SelectTrigger variant="outline" size="md">
-            <SelectInput placeholder="Select transaction type" />
-            <SelectIcon className="mr-3" as={ChevronDownIcon} />
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              <SelectItem label="FOK" value="FOK" />
-            </SelectContent>
-          </SelectPortal>
-        </Select>
-
-        <Box className="flex-row items-center gap-2">
-          <TouchableOpacity onPress={() => handleUSDTamountChange((+USDTamount - 1).toString())}>
-            <IC_Minus className="h-4 w-4" />
-          </TouchableOpacity>
-          <TextInput
-            value={USDTamount}
-            keyboardType="numeric"
-            placeholder="Amount (USDT)"
-            onChangeText={handleUSDTamountChange}
-          />
-          <TouchableOpacity onPress={() => handleUSDTamountChange((+USDTamount + 1).toString())}>
-            <IC_Plus className="h-4 w-4" />
-          </TouchableOpacity>
-        </Box>
-
-        <TouchableOpacity className="bg-green-500 p-2" onPress={handleBuying}>
-          <Text>
-            Buy {newCurrencyAmount}
-            {currentCurrencySymbol}
-          </Text>
-        </TouchableOpacity>
-      </Box>
+      </TouchableWithoutFeedback>
     </>
   );
 }
--- screens/market/BuySellSheet.tsx
+++ screens/market/BuySellSheet.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Box } from '@/components/ui/box'
-import { Text } from '@/components/ui/text'
-import { Actionsheet, ActionsheetBackdrop, ActionsheetContent, ActionsheetDragIndicator, ActionsheetDragIndicatorWrapper, ActionsheetItem, ActionsheetItemText } from '@/components/ui/actionsheet';
+import React from 'react';
+import { Box } from '@/components/ui/box';
+import { Text } from '@/components/ui/text';
+import {
+  Actionsheet,
+  ActionsheetBackdrop,
+  ActionsheetContent,
+  ActionsheetDragIndicator,
+  ActionsheetDragIndicatorWrapper,
+  ActionsheetItem,
+  ActionsheetItemText,
+} from '@/components/ui/actionsheet';
 import { useTheme } from '@/utils/Themes/ThemeProvider';
 import { CryptoData } from '@/utils/api/internal/sql/handleSQLite';
 import CardUpRounded from '@/components/CardUpRounded';
@@ -9,66 +17,65 @@ import { IC_BTCUSDT, IC_Options } from '@/utils/constants/Icons';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { formatSymbol } from '@/utils/functions/help';
 import BuySellOrderBook from '@/components/BuySellOrderBook';
+import BuyNSell from '@/components/BuyNSell';
 
 interface BuySellSheetProps {
-    isOpen: boolean;
-    onClose: () => void;
-    coinData: CryptoData;
+  isOpen: boolean;
+  onClose: () => void;
+  coinData: CryptoData;
 }
 
 const BuySellSheet = ({ isOpen, onClose, coinData }: BuySellSheetProps) => {
-    const { appliedTheme } = useTheme();
+  const { appliedTheme } = useTheme();
 
-    return (
-        <Actionsheet isOpen={isOpen} onClose={onClose} snapPoints={[90]}>
-        <ActionsheetBackdrop />
+  return (
+    <Actionsheet isOpen={isOpen} onClose={onClose} snapPoints={[90]}>
+      <ActionsheetBackdrop />
 
-        <ActionsheetContent className={`bg-background-${appliedTheme} px-0`}>
-            <ActionsheetDragIndicatorWrapper>
-                <ActionsheetDragIndicator />
-            </ActionsheetDragIndicatorWrapper>
+      <ActionsheetContent className={`bg-background-${appliedTheme} px-0`}>
+        <ActionsheetDragIndicatorWrapper>
+          <ActionsheetDragIndicator />
+        </ActionsheetDragIndicatorWrapper>
 
-            {/* Title */}
-            <Box className='flex-row w-full px-4 py-2 justify-between items-center'>
-                <Box className='flex-row items-center gap-3'>
-                    <IC_BTCUSDT className='w-7 h-7' />
-                    <Text className={`text-text-${appliedTheme} font-bold my-2 text-xl`}>
-                        {formatSymbol(coinData.symbol)}
-                    </Text>
-                    <Text className={`${Number(coinData.change) > 0 ? "text-green-500" : "text-red-500"}`}>
-                        {Number(coinData.change) > 0 ? '+':''} {coinData.change}%
-                    </Text>
-                </Box>
-                {/* Icons */}
-                <Box className='flex-row gap-4 items-center'>
-                    <Icon name="candlestick-chart" size={24} color="gray" />
-                    <Icon name="attach-money" size={24} color="gray" />
-                    <IC_Options className='w-5 h-5'/>
-                </Box>
+        {/* Title */}
+        <Box className="w-full flex-row items-center justify-between px-4 py-2">
+          <Box className="flex-row items-center gap-3">
+            <IC_BTCUSDT className="h-7 w-7" />
+            <Text className={`text-text-${appliedTheme} my-2 text-xl font-bold`}>
+              {formatSymbol(coinData.symbol)}
+            </Text>
+            <Text className={`${Number(coinData.change) > 0 ? 'text-green-500' : 'text-red-500'}`}>
+              {Number(coinData.change) > 0 ? '+' : ''} {coinData.change}%
+            </Text>
+          </Box>
+          {/* Icons */}
+          <Box className="flex-row items-center gap-4">
+            <Icon name="candlestick-chart" size={24} color="gray" />
+            <Icon name="attach-money" size={24} color="gray" />
+            <IC_Options className="h-5 w-5" />
+          </Box>
+        </Box>
+
+        <CardUpRounded className="mt-0 rounded-2xl" variant="card">
+          <Box className="flex-1">
+            {/* Top Section */}
+            <Box className="h-3/4 flex-row">
+              <Box className="h-full w-[50%] rounded-l-lg bg-gray-700">
+                <BuySellOrderBook />
+              </Box>
+              <Box className="w-[50%] rounded-r-lg bg-gray-800">
+                <BuyNSell
+                  currentCurrencySymbol={coinData.symbol}
+                  currentCurrencyValue={coinData.price as string}
+                  onClose={onClose}
+                />
+              </Box>
             </Box>
-            
-
-            <CardUpRounded className='mt-0 rounded-2xl' variant='card'>
-                
-                <Box className="flex-1">
-                    {/* Top Section */}
-                    <Box className='flex-row h-3/4'>
-                        <Box className='w-[50%] rounded-l-lg h-full bg-gray-700'>
-                            <BuySellOrderBook />
-                        </Box>
-                        <Box className='w-[50%] rounded-r-lg bg-gray-800'>
-                        
-                        </Box>
-                    </Box>
-
-                </Box>
-            </CardUpRounded>
-            
-
-        </ActionsheetContent>
-        
+          </Box>
+        </CardUpRounded>
+      </ActionsheetContent>
     </Actionsheet>
-    )
-}
+  );
+};
 
 export default BuySellSheet;
